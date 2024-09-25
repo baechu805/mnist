5d3d85274d04337e86a024fba97924cd36b97c0a
baechu805 <bjoo606@naver.com>
2024-09-25 09:27:52 +0900

    fix

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -3,22 +3,47 @@ from mnist.db import select, dml
 import random
 import os
 import requests
+import numpy as np
+from PIL import Image
+from keras.models import load_model
 
 def get_job_img_task():
-   sql = """
-   SELECT 
+    sql = """
+    SELECT 
     num, file_name, file_path
-   FROM image_processing
-   WHERE prediction_result IS NULL
-   ORDER BY num -- 가장 오래된 요청
-   LIMIT 1 -- 하나씩
-   """
-   r = select(sql, 1)
-   
-   if len(r) > 0:
-       return r[0]
-   else:
-       return None
+    FROM image_processing
+    WHERE prediction_result IS NULL
+    ORDER BY num -- 가장 오래된 요청
+    LIMIT 1 -- 하나씩
+    """
+    r = select(sql, 1)
+    if len(r) > 0:
+        return r[0]
+    else:
+        return None
+# 모델 로드
+model = load_model('home/joo/code/mnist/note/mnist240924.keras')  # 학습된 모델 파일 경로
+
+# 사용자 이미지 불러오기 및 전처리
+def preprocess_image(image_path):
+    img = Image.open(image_path).convert('L')  # 흑백 이미지로 변환
+    img = img.resize((28, 28))  # 크기 조정
+
+    # 흑백 반전
+    img = 255 - np.array(img)  # 흑백 반전
+    
+    img = np.array(img)
+    img = img.reshape(1, 28, 28, 1)  # 모델 입력 형태에 맞게 변형
+    img = img / 255.0  # 정규화
+    return img
+
+# 예측
+def predict_digit(image_path):
+    img = preprocess_image(image_path)
+    prediction = model.predict(img)
+    digit = np.argmax(prediction)
+    return digit
+    
 
 def prediction(file_path, num):
     sql = """UPDATE image_processing
@@ -27,7 +52,9 @@ def prediction(file_path, num):
         prediction_time=%s
     WHERE num=%s
     """
-    presult = random.randint(0, 9)
+    job=get_job_img_task()
+    file_path=job['file_path']
+    presult = predict_digit(file_path)
     dml(sql, presult, jigeum.seoul.now(), num)
 
     return presult
@@ -59,12 +86,12 @@ def run():
 
   print(jigeum.seoul.now())
 
-def send_line_noti(file_name, presult):
+  url = "https://notify-api.line.me/api/notify"
+  KEY =  os.getenv('API_TOKEN','false')
+  headers = {"Authorization": "Bearer " + KEY}
+  data = {"message": "성공적으로 저장했습니다!"}
+  headers = {"Authorization": "Bearer " + KEY}
+  response = requests.post(url, data=data, headers=headers)
+  print(response.text)
+  print("SEND LINE NOTI")
 
-    KEY = os.environ.get('API_TOKEN')
-    url = "https://notify-api.line.me/api/notify"
-    data = {"message": "성공적으로 저장했습니다!"}
-    headers = {"Authorization": "Bearer " + KEY}
-    response = requests.post(url, data=data, headers=headers)
-    print(response.text)
-    print("SEND LINE NOTI")
